7e93079074356042648430febb4c0acb5581f9a0
Fiorela <clafiorelariza@gmail.com>
2024-05-17 12:48:58 -0500

    archivo .py con la funcion del primer punto

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+import json
+import os
+
+app = Flask(__name__)
+
+# Cargar datos desde el archivo JSON
+def cargar_datos():
+    with open('data/data.json') as f:
+        return json.load(f)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/regiones', methods=['GET'])
+def regiones():
+    datos = cargar_datos()
+    regiones = [item['region'] for item in datos]
+    return jsonify(regiones)
+
+if __name__ == '__main__':
+    app.run(debug=True)
