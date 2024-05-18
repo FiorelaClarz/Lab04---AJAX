52ece33ec3ea932f4adcbc3a982f47cd69d182bd
Fiorela <clafiorelariza@gmail.com>
2024-05-18 15:38:11 -0500

    añadiedo el punto 7; se hicieron los cambios en los otros archivos en cuanto al formato

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import json
-# import os
+import os
+
+# para visualizar la grafia del punto 5 se tuvo que hacer pip install de plotly en la terminal
 import plotly.graph_objs as go
 
 app = Flask(__name__)
@@ -10,6 +12,7 @@ def cargar_datos():
     with open('data/data.json') as f:
         return json.load(f)
 
+# Index donde se ve el desarrollo de cada punto del lab
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -30,6 +33,28 @@ def totales_por_region():
 def top_totales_por_region(): 
     top_totales = top_10_regiones()
     return render_template('punto3.html', totales=top_totales)
+
+# ir al punto 4
+@app.route('/grafico_arequipa')
+def grafico_arequipa():
+    datos = cargar_datos()
+    for region in datos:
+        if region['region'] == 'Arequipa':
+            fechas = [caso['date'] for caso in region['confirmed']]
+            valores = [int(caso['value']) for caso in region['confirmed']]
+            break
+    return render_template('punto4.html', fechas=json.dumps(fechas), valores=json.dumps(valores))
+
+# ir al punto 5
+@app.route('/grafico_todas_regiones')
+def mostrar_grafico_todas_regiones():
+    grafico_html = grafico_todas_regiones()
+    return render_template('punto5.html', grafico_html=grafico_html)
+
+# ir al punto 7
+@app.route('/comparar_regiones')
+def comparar_regiones():
+    return render_template('punto7.html')
 
 # Regiones
 @app.route('/regiones', methods=['GET'])
@@ -81,35 +106,6 @@ def top_10_regiones():
 
     return top_10
 
-@app.route('/grafico_arequipa')
-def grafico_arequipa():
-    datos = cargar_datos()
-    for region in datos:
-        if region['region'] == 'Arequipa':
-            fechas = [caso['date'] for caso in region['confirmed']]
-            valores = [int(caso['value']) for caso in region['confirmed']]
-            break
-    return render_template('punto4.html', fechas=json.dumps(fechas), valores=json.dumps(valores))
-
-
-# Función para generar el gráfico de líneas de todas las regiones
-# def grafico_todas_regiones():
-#     datos = cargar_datos()
-#     fechas = [entry['date'] for entry in datos[0]['confirmed']]  # Tomamos las fechas de la primera región
-#     regiones = [region['region'] for region in datos]
-#     datos_confirmados = {region['region']: [int(entry['value']) for entry in region['confirmed']] for region in datos}
-
-#     grafico_html = '<div>'
-#     for region, casos in datos_confirmados.items():
-#         grafico_html += f'<h2>{region}</h2>'
-#         grafico_html += '<ul>'
-#         for fecha, caso in zip(fechas, casos):
-#             grafico_html += f'<li>{fecha}: {caso}</li>'
-#         grafico_html += '</ul>'
-#     grafico_html += '</div>'
-
-#     return grafico_html
-
 # Función para generar el gráfico de líneas de todas las regiones
 def grafico_todas_regiones():
     datos = cargar_datos()
@@ -129,10 +125,41 @@ def grafico_todas_regiones():
     return fig.to_html(full_html=False, include_plotlyjs='cdn')
 
 
-@app.route('/grafico_todas_regiones')
-def mostrar_grafico_todas_regiones():
-    grafico_html = grafico_todas_regiones()
-    return render_template('punto5.html', grafico_html=grafico_html)
+# Funcion que permite visualizar la comparacion de losc asos confirmados de dos regiones
+@app.route('/graficar', methods=['POST'])
+def graficar():
+    datos = cargar_datos()
+    region1 = request.json['region1']
+    region2 = request.json['region2']
+
+    region1_data = next(item for item in datos if item['region'] == region1)
+    region2_data = next(item for item in datos if item['region'] == region2)
+
+    fechas = []
+    for data in region1_data['confirmed'] + region2_data['confirmed']:
+        if data['date'] not in fechas:
+            fechas.append(data['date'])
+    fechas.sort()
+
+    def preparar_datos(region_data):
+        datos = {fecha: None for fecha in fechas}
+        for data in region_data['confirmed']:
+            datos[data['date']] = int(data['value'])
+        return [datos[fecha] for fecha in fechas]
+
+    region1_vals = preparar_datos(region1_data)
+    region2_vals = preparar_datos(region2_data)
+
+    datos_grafico = {
+        "labels": fechas,
+        "datasets": [
+            {"label": region1, "data": region1_vals, "borderColor": '#'+format(int(os.urandom(1).hex(), 16)*111111, '06x'), "fill": False},
+            {"label": region2, "data": region2_vals, "borderColor": '#'+format(int(os.urandom(1).hex(), 16)*111111, '06x'), "fill": False}
+        ]
+    }
+
+    return jsonify(datos_grafico)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
