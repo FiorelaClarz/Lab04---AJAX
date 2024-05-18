f3042c98ce0e348fd352c53f7073969ba2453e97
Jherson <jinca@unsa.edu.pe>
2024-05-18 11:30:41 -0500

    Mostrar el contenido de los archivos

--- EjerciciosPropuestos/server.js
+++ EjerciciosPropuestos/server.js
@@ -1,19 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'html')));
-const markdownDir = path.join(__dirname, 'markdown');
+app.use(express.static(path.join(__dirname,"html")));
+const markdownDir = path.join(__dirname,"markdown");
 
-app.get('/files', (req, res) => {
-    fs.readdir(markdownDir, (err, files) => {
+app.get("/files",(req,res) => {
+    fs.readdir(markdownDir,(err,files) => {
         res.json({files});
+    });
+});
+
+app.get("/file/:name",(req,res) => {
+    const fileName = req.params.name;
+    const filePath = path.join(markdownDir,fileName);
+    fs.readFile(filePath,"utf8",(err,data) => {
+        res.json({content:data});
     });
 });
 
